75658f3945fab493edcd02790eaa6af0eecb2c90
Misael Landero <francisco.landero@dexcom.com>
2025-02-14 17:48:56 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
